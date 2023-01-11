95dd20279e7992e7528797dad825890b8469eef3
vilmarsitiodigital <vilmar@sitiodigital.com.br>
2023-01-11 18:41:08 -0300

    Feat: Project finish

---
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { ServerKafka } from '@nestjs/microservices';
+
+@Injectable()
+export class KafkaConsumerService
+  extends ServerKafka
+  implements OnModuleDestroy
+{
+  constructor() {
+    super({
+      client: {
+        clientId: 'notifications',
+        brokers: [`${process.env.UPSTASH_KAFKA_BROKER}`],
+        sasl: {
+          mechanism: 'scram-sha-256',
+          username: `${process.env.UPSTASH_KAFKA_USERNAME}`,
+          password: `${process.env.UPSTASH_KAFKA_PASSWORD}`,
+        },
+        ssl: true,
+      },
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.close();
+  }
+}
